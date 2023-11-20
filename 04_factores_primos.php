d491b21db9cf56baedea623de45ea6aa7e44ce04
alarcon steven <devalarcon.23@gmail.com>
2023-11-20 15:00:28 -0500

    Factorizacion new

--- 04_factores_primos.php
+++ 04_factores_primos.php
@@ -28,6 +28,10 @@ class PrimeFactor
 
     public function multipliArray(array $array)
     {
+        if (empty($array[0])) {
+            return false;
+        }
+
         $lengt = count($array);
 
         $result = false;
@@ -67,20 +71,71 @@ class PrimeFactor
         }
 
         $var = $this->multipliArray($array);
-        var_dump($var);
-        var_dump($flag);
-
 
         if ($var > $flag) {
             array_pop($array);
         }
 
 
-        print_r($array);
+        return $array;
+    }
+
+    public function factorDetected($num)
+    {
+        $array = [2, 3, 5, 7];
+        $lengt = count($array);
+        $value = false;
+
+        for ($i = 0; $i < $lengt; $i++) {
+
+            if ($num % $array[$i] == 0) {
+                $value = true;
+            }
+        }
+
+        return $value;
+    }
+
+    public function getBigerFactor($array)
+    {
+        $lenght = count($array);
+
+        for ($i = 0; $i < $lenght - 1; $i++) {
+            for ($j = 0; $j < $lenght - 1; $j++) {
+                if ($array[$j] < $array[$j + 1]) {
+                    $var = $array[$j];
+                    $array[$j] = $array[$j + 1];
+                    $array[$j + 1] = $var;
+                }
+            }
+        }
+
+        return $array;
+    }
+
+    public function getFators($num)
+    {
+
+        $result = "Error: El número no se puede factorizar";
+        $value = "";
+
+        if ($num % 2 == 0 || $num % 3 == 0 || $num % 5 == 0 || $num % 7 == 0) {
+
+            $get_result = $this->random($num);
+            $biger = $this->getBigerFactor($get_result);
+
+            foreach ($get_result as $item) {
+                $value .= $item . " x ";
+            }
+
+            $position = strrpos($value, "x");
+            $change = substr($value, 0, $position);
+            $result = $num . " = " . $change . " | " . "Su factor prímo mayor es: " . $biger[0];
+        }
+        return $result;
     }
 }
 
 
 $factors = new PrimeFactor;
-echo $factors->random(12);
-//echo $factors->multipliArray([2, 2, 2]);
+echo $factors->getFators(6000);
---
+++ 05_factorización.php
@@ -0,0 +1,87 @@
+<?php
+
+class Factorizacion
+{
+    public function factoriza($number)
+    {
+        $factores = [];
+
+        while ($number % 2 == 0) {
+            $factores[] = 2;
+            $number /= 2;
+        }
+
+        $factor = 3;
+
+        while ($factor * $factor < $number) {
+            while ($number % $factor == 0) {
+                $factores[] = $factor;
+                $number /= $factor;
+            }
+
+            $factor += 2;
+        }
+
+        if ($number > 1) {
+            $factores[] = $number;
+        }
+
+        return $factores;
+    }
+
+    public function getBigerFactor(array $array)
+    {
+        $lenght = count($array);
+
+        for ($i = 0; $i < $lenght; $i++) {
+            for ($j = 0; $j < $lenght - 1; $j++) {
+                if ($array[$j] < $array[$j + 1]) {
+                    $var = $array[$j];
+                    $array[$j] = $array[$j + 1];
+                    $var = $array[$j + 1];
+                }
+            }
+        }
+
+        return $array;
+    }
+
+    public function multipliFActors(array $array)
+    {
+        $count = count($array);
+        $i = 0;
+        $result = 0;
+
+        for ($i = 0; $i < $count - 1; $i++) {
+            $result = $array[$i] * $array[$i + 1];
+        }
+
+
+        return $result;
+    }
+
+    public function printValues($number)
+    {
+
+        $sheing = "";
+        $result = "";
+        $big = $this->getBigerFactor($this->factoriza($number));
+        $mult = $this->multipliFActors($this->factoriza($number));
+
+
+
+        foreach ($this->factoriza($number) as $value) {
+            $sheing .= $value . " x ";
+        }
+
+        $position = strrpos($sheing, "x");
+        $new_sheing = substr($sheing, 0, $position);
+        $result = $number . " = " . $new_sheing . " | Su factor primo mayor es: " . $big[0] . "\n";
+        $result .= "Multiplicación random: " . $mult;
+
+        return $result;
+    }
+}
+
+$factorizarcion = new Factorizacion;
+echo $factorizarcion->printValues(28) . "\n";
